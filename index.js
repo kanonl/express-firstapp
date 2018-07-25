d49fa7ddbe5f225bfc9643f9e6ca4a4f6efe9c3a
Kanon Lo <15108745+kanonl@users.noreply.github.com>
2018-07-25 13:03:28 -0400

    Added contact us page with mailgun integration

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express'),
     app = express(),
+    compression = require('compression'),
     path = require('path'),
     indexRouter = require('./routes/index'),
-    accountRouter = require('./routes/account');
+    accountRouter = require('./routes/account'),
+    contactRouter = require('./routes/contact');
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/account', accountRouter);
+app.use('/contact', contactRouter);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');

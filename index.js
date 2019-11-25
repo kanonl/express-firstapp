e7a9734bd49b8536befdc26f525d11c3b8feab1f
Kanon Lo <15108745+kanonl@users.noreply.github.com>
2019-11-25 09:00:24 -0500

    Dependencies update

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express'),
     app = express(),
     compression = require('compression'),
+    helmet = require('helmet'),
     path = require('path'),
     indexRouter = require('./routes/index'),
     accountRouter = require('./routes/account'),
     contactRouter = require('./routes/contact');
 
+app.use(helmet());
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
@@ -15,4 +17,4 @@ app.use('/contact', contactRouter);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3030);
